0cf9b692bd41fbd846b9c7b5e9179551061a0e98
hajar-b <hajar.bouziane@outlook.com>
2021-05-15 17:04:32 +0200

    Blockchaine

--- Mineur.java
+++ Mineur.java
@@ -11,32 +11,25 @@ public class Mineur {
 	private String ip;
 	private int port;
 	private Socket socket;
-	//private MulticastSocket multicast;
-	//private InetAddress group;
 	private DataOutputStream out;
 	private DataInputStream in;
+	private MulticastSocket multicast;
+	private InetAddress group;
 	
 	public Mineur(String ip, int port) throws IOException {
 		this.ip = ip;
 		this.port = port;
 		this.socket = new Socket(this.ip, this.port);
 		
-		//multicast = new MulticastSocket(this.port);
-		//group = InetAddress.getByName("225.4.5.6");
-		//multicast.joinGroup(this.group);
+		multicast = new MulticastSocket(this.port);
+		group = InetAddress.getByName("225.4.5.6");
+		multicast.joinGroup(this.group);
+		
 		out = new DataOutputStream(socket.getOutputStream());
 		in = new DataInputStream(System.in);
 
 	}
-	/*
-	public void printMessage() throws IOException {
-		byte[] message = new byte[256];
-		DatagramPacket packet = new DatagramPacket(message, message.length);
-		multicast.receive(packet);
-		System.out.println(new String(packet.getData()));
-	}
-	*/
-	
+		
 	
 	// dans ce tableau, je met les 2 chiffres que le client envoie dans la transaction. Pour tester si la transaction est correcte, il suffit de prendre les 3 chiffres stocké dans ce tableau. 
 	// Si besoin, on peut mettre le tableau dans une variable global de la classe ou juste le faire passer en argument d'une fonction qui teste si la transaction est correct et pour faire le calcul du sel
@@ -58,9 +51,29 @@ public class Mineur {
 				System.out.println("Veuillez saisir 3 entiers separe par des espaces pour votre transaction");
 		}
 	}
+
+	public void printMessage() throws IOException {
+		byte[] message = new byte[256];
+		DatagramPacket packet = new DatagramPacket(message, message.length);
+		multicast.receive(packet);
+		System.out.println(new String(packet.getData()));
+	}
 	
 	public static void main(String[] args) throws IOException {
 		Mineur min = new Mineur("localhost",3333);
+		Thread t = new Thread(new Runnable(){
+			@Override
+			public void run(){
+				while(true){
+					try {
+						min.printMessage();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		t.start();
 		min.manageRequest();		
 	}
 
--- ServeurCentralHandler.java
+++ ServeurCentralHandler.java
@@ -6,12 +6,12 @@ import java.net.Socket;
 public class ServeurCentralHandler implements Runnable {
 	private Socket mineur;
 	private DataInputStream in;
-	
+	private Multicast mult;
 	
 	public ServeurCentralHandler(Socket mineur) throws IOException {
 		this.mineur = mineur;
 		in = new DataInputStream(this.mineur.getInputStream());
-		
+		mult = new Multicast();
 	}
 	
 	@Override
@@ -20,6 +20,12 @@ public class ServeurCentralHandler implements Runnable {
 			try{
 				String request = in.readUTF();
 				System.out.println(request);
+				if(request.equals("1 1 1")){
+					System.out.println("Début du multicast");
+					byte[] message = ("Le serveur vous parle ...").getBytes();
+					mult.send(message);
+					System.out.println("Fin du multicast");
+				}
 			} catch(IOException e) {
 				System.out.println(e);
 			}
